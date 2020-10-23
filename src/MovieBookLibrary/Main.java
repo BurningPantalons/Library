f6060c600ee2d99f9b92070858ad550b66474a7f
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2020-10-23 12:45:45 +0200

    update IF-QUIT in main

--- src/MovieBookLibrary/Main.java
+++ src/MovieBookLibrary/Main.java
@@ -1,11 +1,24 @@
 package MovieBookLibrary;
 import java.util.*;
 
+import examinerandeUppgifter.mineSweeper.Command;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		
+		while (true) {
+			
+			String userInput = scanner.nextLine();
+	        Command command = parseCommand(userInput);
+
+			if (command == Command.QUIT) {
+				System.exit(0);
+			}
+			scanner.close();
+		}
 		
 		/*läsa in csv.filer när programmet startar
 		 * parseCommand som tar reda på rätt kommando och kallar på metod
@@ -13,7 +26,7 @@ public class Main {
 		 * list() som visar en lista på Böcker och Filmer och ifall dom är utlånade (till specificerad kund)
 		 */
 
-		scanner.close();
+		
 
 	}
 }
