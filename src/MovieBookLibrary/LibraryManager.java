4767cc5d553a620d9a46bab76ab97b9bb60cf207
BurningPantalons <70105297+BurningPantalons@users.noreply.github.com>
2020-11-09 18:35:07 +0100

    Moved logic from searchLibrary
    to methods Info, Deregister, Checkin, Checkout

--- src/MovieBookLibrary/LibraryManager.java
+++ src/MovieBookLibrary/LibraryManager.java
@@ -17,11 +17,6 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 	private String movBookPath;
 
 	public LibraryManager(String movBookPath) throws FileNotFoundException {
-		try {
-			writeCsvFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		this.movBookPath = movBookPath;
 		parseItems(movBookPath);
 	}
@@ -69,6 +64,7 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 				e.printStackTrace();
 			}
 			libraryList.add(new Book(id, title, value, author, nPages));
+			sc.close();
 			try {
 				writeItems();
 			} catch (IOException e) {
@@ -91,6 +87,7 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 				e.printStackTrace();
 			}
 			libraryList.add(new Movie(id, title, value, rating, runtime));
+			sc.close();
 			try {
 				writeItems();
 			} catch (IOException e) {
@@ -104,46 +101,52 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 			register(newArg);
 			return;
 		}
-
+		
 	}
 
 	private List<Item> parseItems(String movBookPath) throws FileNotFoundException {
 
 		FileReader reader = new FileReader(movBookPath);
 		Scanner scanner = new Scanner(reader);
-		List<Item> fileList = new ArrayList<Item>();
 
 		if (movBookPath.isBlank()) {
 			System.out.println("Could not find any information on items to parse.\nProceeding to start application.");
 			scanner.close();
 			return null;
-		}
-		try {
-			while (scanner.hasNextLine()) {
-				for (Item item : fileList) {
+		} else
+			try {
+				while (scanner.hasNextLine()) {
 					String csvRecord = scanner.nextLine();
-					if (csvRecord.contentEquals(Movie.getCsvHeaderString())
-							|| csvRecord.contentEquals(Book.getCsvHeaderString())) {
+					String[] recordValues = csvRecord.split(",");
+					if (csvRecord.equals(Movie.getCsvHeaderString()) || csvRecord.equals(Book.getCsvHeaderString())) {
 						scanner.nextLine();
-					} else if (item.getTypeIdentifier().contentEquals("(Book)")) {
-						Book.parseBook(csvRecord);
-						libraryList.add(item);
-					} else if (item.getTypeIdentifier().contentEquals("(Movie)"))
-						Movie.parseMovie(csvRecord);
-					libraryList.add(item);
+					} else if (recordValues[0].equals(TYPE_BOOK)) {						
+						int id = Integer.parseInt(recordValues[1]);
+						String title = recordValues[2];
+						int value = Integer.parseInt(recordValues[3]);
+						String author = recordValues[4];
+						int nPages = Integer.parseInt(recordValues[5]);
+						Book book = new Book(id, title, value, author, nPages);
+						libraryList.add(book);
+					} else if (recordValues[0].equals(TYPE_MOVIE)) {						
+						int id = Integer.parseInt(recordValues[1]);
+						String title = recordValues[2];
+						int value = Integer.parseInt(recordValues[3]);
+						float rating = Float.parseFloat(recordValues[4]);
+						int runtime = Integer.parseInt(recordValues[5]);
+						Movie movie = new Movie(id, title, value, rating, runtime);
+						libraryList.add(movie);
+					}
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("parseItems");
 			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("parseItems");
-		}
 		scanner.close();
-		//list();
-		return fileList;
+		return null;
 	}
 
-	private void writeCsvFile() throws IOException {
+	private void createCsvFile() throws IOException {
 		CSVPrinter pW = new CSVPrinter(new FileWriter("MovieBookLibrary.csv"), CSVFormat.DEFAULT);
 		String bookHeader = Book.getCsvHeaderString();
 		String movieHeader = Movie.getCsvHeaderString();
@@ -157,9 +160,9 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 
 		try {
 			for (Item item : libraryList) {
-				if (item.getTypeIdentifier().contentEquals("(Book)")) {
+				if (item.getTypeIdentifier().equals(TYPE_BOOK)) {
 					pW.printRecord(item);
-				} else if (item.getTypeIdentifier().contentEquals("(Movie)")) {
+				} else if (item.getTypeIdentifier().equals(TYPE_MOVIE)) {
 					pW.printRecord(item);
 				} else {
 
@@ -177,90 +180,133 @@ public class LibraryManager extends Exceptions implements IntLibrary<Item>, Item
 
 	}
 
-	private int searchLibrary(String argument) {
-		try {
-			int Id = Integer.parseInt(argument);
-			for (int i = 0; i > libraryList.size();) {
-				Item item = libraryList.get(i);
-				i++;
-				if (Id == item.getId()) {
-					return Id;
-				} else {
-					System.out.println("Item Id does not exist.\n");
-					return 0;
-				}
-			}
+	public void searchLibrary(String argument) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Id number:\n>");
+		int Id = Integer.parseInt(sc.next());
 
-		} catch (Exception e) {
-			System.out.println("Failed to parse index from argument.\nTry again");
-			return 0;
+		for (int i = 0; i > libraryList.size();) {
+			Item item = libraryList.get(i);
+			if (Integer.valueOf(item.getId()) == Id) {
+				if (argument.equals("Info")) {
+					int Index = libraryList.indexOf(item);
+					info(Index);
+				}
+				else if (argument.equals("Checkin")) {
+					int Index = libraryList.indexOf(item);
+					checkin(Index);
+				}
+				else if (argument.equals("Checkout")) {
+					int Index = libraryList.indexOf(item);
+					checkin(Index);
+				}
+				else if (argument.equals("Deregister")) {
+					int Index = libraryList.indexOf(item);
+					deregister(Index);
+				}
+				
+			} else if (libraryList.contains(Id) != EXIST) {
+				System.out.println("Item Id does not exist.\n");
+				return;
+			} else {
+				System.out.println("Failed to parse index from argument.\nTry again");
+				return;
+
+			}
+			i++;
 		}
-		return 0;
+		System.out.println("searchLibrary-end");
+		return;
 	}
 
 	@Override
-	public void checkout(String argument) {
-		Scanner customerSc = new Scanner(System.in);
-		String Id = argument;
-		searchLibrary(Id);
-		if (libraryList.contains(Id) == EXIST) {
+	public void checkout(int Id) {
+		int index = Id;
+		Item item = libraryList.get(index);
+		if (item.stateIdentifier == STATE_NOT_AVAILABLE) {
+			System.out.println("Item already borrowed out");
+			return;
+		} else {
+			System.out.println("checkout-borrow");
+			Scanner customerSc = new Scanner(System.in);
 			String customerName, customerNumber;
 			System.out.println("Enter name for person borrowing this item:");
 			customerName = customerSc.nextLine();
 			System.out.println("Enter number for person borrowing this item:");
 			customerNumber = customerSc.nextLine();
-
+			item.stateIdentifier = STATE_NOT_AVAILABLE;
+			item.setCustomerName(customerName);
+			item.setCustomerNumber(customerNumber);
+			System.out.println("Item borrowed to: " + customerName + " number: " + customerNumber);
+			customerSc.close();
+			return;
 		}
-
 	}
-	/*
-	 * deklarera variablen metod som itererar genom listan efter id - SÖKMETOD
-	 * skickar index borrowed = true customer name, nr
-	 */
 
 	@Override
-	public void checkin(String argument) {
-		String Id = argument;
-		searchLibrary(Id);
-		/*
-		 * deklarera variablen metod som itererar genom listan efter id - SÖKMETOD
-		 * skickar index borrowed = true, ta bort customer name, nr
-		 */
+	public void checkin(int Id) {
+		int index = Id;
+		Item item = libraryList.get(index);
+		if (item.stateIdentifier != STATE_NOT_AVAILABLE) {
+			System.out.println("Item is already in stock.");
+			return;
+		} else {
+			item.stateIdentifier = STATE_IN_STOCK;
+			item.setCustomerName(null);
+			item.setCustomerNumber(null);
+			System.out.println("Item returned to the library.");
+			return;
+		}
+
 	}
 
 	@Override
 	public void deregister(int index) {
-		int Id = index;
-		searchLibrary(Integer.toString(Id));
-		libraryList.remove(Id);
+		int deleteId = index;
+		Item deleteItem = libraryList.get(deleteId);
+		libraryList.remove(deleteItem);
 		System.out.println("Item removed from library.");
 		return;
 	}
 
 	@Override
-	public void info(String argument) {
-		String Id = argument;
-		searchLibrary(Id);
-
+	public void info(int Id) {
+		int index = Id;
+		Item item = libraryList.get(index);
+		if (item.stateIdentifier == STATE_IN_STOCK) {
+			item.toString();
+			System.out.println("info-in-stock");
+			return;
+		} else if (item.stateIdentifier == STATE_NOT_AVAILABLE) {
+			System.out.printf(
+					item.toString() + "Borrowed by: " + item.getCustomerName() + "number: " + item.getCustomerNumber());
+			System.out.println("info-borrowed");
+			return;
+		}
 		return;
 	}
 
 	@Override
-	public void list() {
-		if (libraryList.isEmpty() == true) {
-			System.out.println("The list is empty.");
-			System.out.println("Enter command:\nFor help, write help + enter.");
-			return;
-		} else {
-			for (Item item : libraryList) {
-				if (item.stateIdentifier == STATE_IN_STOCK) {
-					System.out.printf("%s,%d,%s,%s",item.typeIdentifier, item.id, item.title, STATE_IN_STOCK);
-				} else if (item.stateIdentifier == STATE_NOT_AVAILABLE) {
-					System.out.printf("%s,%d,%s,\nBorrowed by: %s, %s",item.typeIdentifier, item.id, item.title, item.customerName,
-							item.customerNumber);
+	public void list() throws NullPointerException {
+		try {
+			if (libraryList.isEmpty()) {
+				System.out.println("The list is empty.");
+				System.out.println("Enter command:\nFor help, write help + enter.");
+				return;
+			} else {
+				for (Item item : libraryList) {
+					if (item.stateIdentifier == STATE_IN_STOCK) {
+						System.out.printf("%s,%d,%s,%s\n", item.typeIdentifier, item.id, item.title, STATE_IN_STOCK);
+					} else if (item.stateIdentifier == STATE_NOT_AVAILABLE) {
+						System.out.printf("%s,%d,%s,\nBorrowed by: %s, %s\n", item.typeIdentifier, item.id, item.title,
+								item.customerName, item.customerNumber);
+					}
 				}
+				System.out.println("Enter command:\nFor help, write help + enter.\n");
+				return;
 			}
-			System.out.println("Enter command:\nFor help, write help + enter.");
+		} catch (NullPointerException e) {
+			e.printStackTrace();
 			return;
 		}
 	}
